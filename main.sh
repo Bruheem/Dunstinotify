1911b2e5bf9017116cc18a425d5fc3a12c1ba487
Bruheem <brqhim@pm.me>
2022-08-04 14:49:01 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,8 +2,10 @@
 
 memory=$(free | grep Mem | cut -f 20 -d ' ')
 battery=$(acpi | cut -f 4 -d ' ' | sed 's/%/ /')
+temperature=$(sensors | grep temp1: | cut -f 9 -d ' ' | cut -c 2- | sed 's/°C/ /')
 
 while [[ true ]]; do
+
 	if [[ $memory<=100000 ]]; then
 		dunstify "Warning!" "High memory usage"
 	fi
@@ -12,7 +14,10 @@ while [[ true ]]; do
 		dunstify "Warning!" "Battery fully charged. Unplug the charger"
 		elif [[ $battery<=20 ]]; then
 			dunstify "Warning!" "Low battery. Plug the charger"
+	fi
 
+	if [[ $temperature>=85 ]]; then
+		dunstify "Warning!" "High temperature recorded"
 	fi
 
 	sleep 1m
