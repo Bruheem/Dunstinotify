c9bc2382d0cd124b5450816ca21091e132198dee
Bruheem <brqhim@pm.me>
2022-08-04 14:09:41 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 memory=$(free | grep Mem | cut -f 20 -d ' ')
-
+battery=$(acpi | cut -f 4 -d ' ' | sed 's/%/ /')
 
 while [[ true ]]; do
 	if [[ $memory<=100000 ]]; then
-	dunstify "Warning!" "High memory usage"
+		dunstify "Warning!" "High memory usage"
+	fi
+
+	if [[ $battery=100 ]]; then
+		dunstify "Warning!" "Battery fully charged. Unplug the charger"
+		elif [[ $battery<=20 ]]; then
+			dunstify "Warning!" "Low battery. Plug the charger"
+
 	fi
 
 	sleep 1m
